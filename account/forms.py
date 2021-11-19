a32aaaf10ad8f33c63dd6dee40283f53758e5dd5
mosihere <mostafakhoshhal@yahoo.com>
2021-11-19 20:43:09 +0330

    login/register responsive

--- account/forms.py
+++ account/forms.py
@@ -1,8 +1,6 @@
 from django.core.exceptions import ValidationError
 from django import forms
 from django.contrib.auth.models import User
-from django.forms import widgets
-from django.http.request import validate_host
 
 
 
