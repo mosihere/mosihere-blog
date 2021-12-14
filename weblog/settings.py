29caaf4f2eac5d7095fb6fe207aecf87c5f42801
mosihere <mostafakhoshhal@yahoo.com>
2021-12-15 01:20:52 +0330

    settings updated

--- weblog/settings.py
+++ weblog/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATUC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
