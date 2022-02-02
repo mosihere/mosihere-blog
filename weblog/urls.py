d4ea76fa27bd93fe564ccde32ce0cc238cc2dc0e
mosihere <mostafakhoshhal@yahoo.com>
2022-02-02 21:18:07 +0330

    django-debug-toolbar added recently
    Signed-off-by: mosihere <mostafakhoshhal@yahoo.com>

--- weblog/urls.py
+++ weblog/urls.py
@@ -1,22 +1,9 @@
-"""weblog URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +11,7 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('contact_us/', include('contact_us.urls')),
     path('', include('home.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
 
 if settings.DEBUG:
